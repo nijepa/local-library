717f41b53d498076721dc4713d9e90abfc5c261b
nijepa <nijepa@gmail.com>
2020-08-13 10:13:54 +0200

    Initial commit

---
+++ models/author.js
@@ -0,0 +1,73 @@
+var mongoose = require('mongoose');
+var moment = require('moment');
+
+var Schema = mongoose.Schema;
+
+var AuthorSchema = new Schema(
+  {
+    first_name: {type: String, required: true, maxlength: 100},
+    family_name: {type: String, required: true, maxlength: 100},
+    date_of_birth: {type: Date},
+    date_of_death: {type: Date},
+  }
+);
+
+// Virtual for author's full name
+AuthorSchema
+.virtual('name')
+.get(function () {
+
+// To avoid errors in cases where an author does not have either a family name or first name
+// We want to make sure we handle the exception by returning an empty string for that case
+
+  var fullname = '';
+  if (this.first_name && this.family_name) {
+    fullname = this.family_name + ', ' + this.first_name
+  }
+  if (!this.first_name || !this.family_name) {
+    fullname = '';
+  }
+
+  return fullname;
+});
+
+// Virtual for author's lifespan
+AuthorSchema
+.virtual('lifespan')
+.get(function () {
+  if (this.date_of_birth && this.date_of_death) {
+    return (this.date_of_death.getYear() - this.date_of_birth.getYear()).toString();
+  }
+});
+
+// Virtual for author's URL
+AuthorSchema
+.virtual('url')
+.get(function () {
+  return '/catalog/author/' + this._id;
+});
+
+AuthorSchema
+.virtual('lifespan_formatted')
+.get(function () {
+  return (this.date_of_death.getYear() - this.date_of_birth.getYear()).toString();
+});
+
+AuthorSchema
+.virtual('date_of_birth_formatted')
+.get(function () {
+  if (this.date_of_birth) {
+    return moment(this.date_of_birth.getYear()).format('MMMM Do, YYYY');
+  }
+});
+
+AuthorSchema
+.virtual('date_of_death_formatted')
+.get(function () {
+  if (this.date_of_death) {
+    return moment(this.date_of_death.getYear()).format('MMMM Do, YYYY');
+  }
+});
+
+//Export model
+module.exports = mongoose.model('Author', AuthorSchema );
---
+++ models/genre.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var GenreSchema = new Schema(
+  {
+    name: {type: String, required: true, minlength: 3, maxlength: 100} //reference to the associated book
+  }
+);
+
+// Virtual for Genre's URL
+GenreSchema
+.virtual('url')
+.get(function () {
+  return '/catalog/genre/' + this._id;
+});
+
+//Export model
+module.exports = mongoose.model('Genre', GenreSchema);
